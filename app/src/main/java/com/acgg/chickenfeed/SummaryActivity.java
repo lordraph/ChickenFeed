bbcb298be949f7da3d7eb0b266fa5332c95de4d1
Tolulope <toogunv@gmail.com>
2018-05-15 15:12:14 +0100

    updated

--- app/src/main/java/com/acgg/chickenfeed/SummaryActivity.java
+++ app/src/main/java/com/acgg/chickenfeed/SummaryActivity.java
@@ -1,6 +1,9 @@
 package com.acgg.chickenfeed;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -10,18 +13,38 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.acgg.chickenfeed.data.ChickenFeedHelper;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class SummaryActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolBar;
+    LinearLayout linearLayout;
+    TableLayout tableLayout;
+    ChickenFeedHelper dbHelper;
+    ArrayList<String> classArr, birdCatArr, ingredientArr, classHeaderArr, resultFeedIngre, resultComment;
+    ArrayList<Integer> energyArr, QtyArr, formulationNoArr, resultqtySpecified;
+    ArrayList<Double> phosphorus, calciumArr, crudeProteinArr, resultProportionPercent, resultProportionUnit;
+    Integer formNo=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
+        dbHelper = new ChickenFeedHelper(this);
+        tableLayout = findViewById(R.id.select_ingredient_mixing_table);
+        linearLayout = findViewById(R.id.summary_View_Layout);
 
         mToolBar = findViewById(R.id.nav_actionBar);
         setSupportActionBar(mToolBar);
@@ -36,6 +59,254 @@ public class SummaryActivity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
         NavigationMenuView navigationMenuView = (NavigationMenuView) navigationView.getChildAt(0);
         navigationMenuView.addItemDecoration(new DividerItemDecoration(SummaryActivity.this, DividerItemDecoration.VERTICAL));
+
+        //get formulation No
+        Intent intent = getIntent();
+        formNo = intent.getIntExtra("formuNo",0);
+
+        loadIngredientForMix();
+        outcomeFromFormulation();
+
+    }
+
+    private void outcomeFromFormulation(){
+        resultFeedIngre = new ArrayList <>();
+        resultProportionPercent = new ArrayList <>();
+        resultProportionUnit = new ArrayList <>();
+        resultqtySpecified = new ArrayList <>();
+        resultComment = new ArrayList <>();
+        classHeaderArr = new ArrayList <>();
+
+
+        // Load the table header
+        classHeaderArr.add("Ingredient");
+        classHeaderArr.add("Proportion(%)");
+        classHeaderArr.add("Proportion");
+        classHeaderArr.add("Quantity Specified");
+        classHeaderArr.add("Comment");
+
+        //get data from database
+
+        Cursor cursor = dbHelper.getFormulationResult(formNo);
+
+        while(cursor.moveToNext()){
+            resultFeedIngre.add(cursor.getString(1));
+            resultProportionPercent.add(cursor.getDouble(3));
+            resultProportionUnit.add(cursor.getDouble(4));
+            resultqtySpecified.add(cursor.getInt(5));
+            resultComment.add(cursor.getString(6));
+        }
+
+
+
+        // create textview for ingredient name
+        TextView ingredienttxt= new TextView(this);
+        ingredienttxt.setText("Outcome from Formulation");
+        ingredienttxt.setTypeface(null, Typeface.BOLD);
+        ingredienttxt.setTextColor(Color.parseColor("#1f7101"));
+        linearLayout.addView(ingredienttxt);
+
+
+        // Create a table
+        TableLayout resultTable = new TableLayout(this);
+        TableLayout.LayoutParams tp = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT);
+        resultTable.setLayoutParams(tp);
+        resultTable.setStretchAllColumns(true);
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        //        populate in a table
+
+        for(int i=0; i<classHeaderArr.size(); i++){
+
+            TableRow new_row = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+            new_row.setLayoutParams(lp);
+
+
+            // create textview for ingredient name
+            TextView ingredientName= new TextView(this);
+            ingredientName.setText(classHeaderArr.get(i));
+            ingredientName.setTypeface(null, Typeface.BOLD);
+
+            if( i%2 == 0){
+                ingredientName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+            }else{
+                ingredientName.setBackgroundColor(Color.parseColor("#4caf50"));
+
+            }
+            new_row.addView(ingredientName);
+
+            for (int eachRow=0; eachRow < resultFeedIngre.size(); eachRow++){
+
+                if(classHeaderArr.get(i).equals("Ingredient")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(resultFeedIngre.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+                    new_row.addView(eachName);
+                }
+
+
+                if(classHeaderArr.get(i).equals("Proportion(%)")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(decimalFormat.format(resultProportionPercent.get(eachRow))));
+                    eachName.setBackgroundColor(Color.parseColor("#4caf50"));
+                    new_row.addView(eachName);
+                }
+
+                if(classHeaderArr.get(i).equals("Proportion")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(decimalFormat.format(resultProportionUnit.get(eachRow))));
+                    eachName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+                    new_row.addView(eachName);
+                }
+
+                if(classHeaderArr.get(i).equals("Quantity Specified")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(resultqtySpecified.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#4caf50"));
+                    new_row.addView(eachName);
+                }
+
+                if(classHeaderArr.get(i).equals("Comment")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(resultComment.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+                    new_row.addView(eachName);
+                }
+
+
+
+
+            }
+            resultTable.addView(new_row);
+        }
+
+
+        linearLayout.addView(resultTable);
+
+    }
+
+
+
+    private void loadIngredientForMix() {
+
+        ingredientArr = new ArrayList<>();
+        formulationNoArr = new ArrayList<>();
+        birdCatArr  = new ArrayList<>();
+        classHeaderArr  = new ArrayList<>();
+        QtyArr = new ArrayList<>();
+        classArr = new ArrayList<>();
+        crudeProteinArr = new ArrayList<>();
+        energyArr  = new ArrayList<>();
+        calciumArr  = new ArrayList<>();
+        phosphorus  = new ArrayList<>();
+
+
+        // Load the header into an array
+
+        classHeaderArr.add("Ingredient");
+        classHeaderArr.add("Class");
+        classHeaderArr.add("Crude Protein");
+        classHeaderArr.add("Calcium");
+        classHeaderArr.add("Phosphorus");
+        classHeaderArr.add("Quantity");
+
+
+        Cursor getIngridient = dbHelper.getIngredient(formNo);
+
+//        Toast.makeText(this, "get count: " + String.valueOf(getIngridient.getCount()), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "get count: " + String.valueOf(formNo), Toast.LENGTH_SHORT).show();
+
+        while(getIngridient.moveToNext()){
+
+            ingredientArr.add(getIngridient.getString(1));
+            QtyArr.add(getIngridient.getInt(4));
+            classArr.add(getIngridient.getString(5));
+            crudeProteinArr.add(getIngridient.getDouble(6));
+            energyArr.add(getIngridient.getInt(7));
+            calciumArr.add(getIngridient.getDouble(8));
+            phosphorus.add(getIngridient.getDouble(9));
+
+        }
+
+//        populate in a table
+
+        for(int i=0; i<classHeaderArr.size(); i++){
+
+            TableRow new_row = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            new_row.setLayoutParams(lp);
+
+
+            // create textview for ingredient name
+            TextView ingredientName = new TextView(this);
+            ingredientName.setText(classHeaderArr.get(i));
+            ingredientName.setTypeface(null, Typeface.BOLD);
+
+            if( i%2 == 0){
+                ingredientName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+            }else{
+                ingredientName.setBackgroundColor(Color.parseColor("#4caf50"));
+
+            }
+            new_row.addView(ingredientName);
+
+            for (int eachRow=0; eachRow < ingredientArr.size(); eachRow++){
+
+                if(classHeaderArr.get(i).equals("Ingredient")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(ingredientArr.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+                    new_row.addView(eachName);
+                }
+
+
+                if(classHeaderArr.get(i).equals("Class")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(classArr.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#4caf50"));
+                    new_row.addView(eachName);
+                }
+
+                if(classHeaderArr.get(i).equals("Crude Protein")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(crudeProteinArr.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+                    new_row.addView(eachName);
+                }
+
+                if(classHeaderArr.get(i).equals("Calcium")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(calciumArr.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#4caf50"));
+                    new_row.addView(eachName);
+                }
+
+                if(classHeaderArr.get(i).equals("Phosphorus")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(phosphorus.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#a5d6a7"));
+                    new_row.addView(eachName);
+                }
+
+                if(classHeaderArr.get(i).equals("Quantity")) {
+                    TextView eachName = new TextView(this);
+                    eachName.setText(String.valueOf(QtyArr.get(eachRow)));
+                    eachName.setBackgroundColor(Color.parseColor("#4caf50"));
+                    new_row.addView(eachName);
+                }
+
+
+
+            }
+            tableLayout.addView(new_row);
+        }
+
+//        Toast.makeText(this, "size "+ ingredientArr.size(), Toast.LENGTH_SHORT).show();
+
+
+
 
     }
 
---
+++ app/src/main/java/com/acgg/chickenfeed/data/ChickenFeedHelper.java
@@ -0,0 +1,278 @@
+package com.acgg.chickenfeed.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ChickenFeedHelper extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "ChickenFeed.db";
+
+
+
+    public ChickenFeedHelper (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+//        Create feed library table
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_CREATE_BUGS_TABLE);
+        db.execSQL(ChickenFeedDbContract.IngredientRecord.SQL_CREATE_INGREDIENT_TABLE);
+        db.execSQL(ChickenFeedDbContract.FormulatedRecord.SQL_CREATE_FORMULATED_RECORD_TABLE);
+        db.execSQL(ChickenFeedDbContract.ResultFormulatedRecord.SQL_CREATE_FORMULATED_RECORD_TABLE);
+
+
+//        insert feed ingredient into table created
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_ACHA);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CASSAVA_PEEL_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CASSAVA_FLOUR);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CASSAVA_LEAF_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CASSAVA_GRIT);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CASSAVA_STARCH);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_COCOYAM);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SORGHUM_WHITE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SORGHUM_RED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_MAIZE_WHITE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_MAIZE_YELLOW);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_MILLET);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_PALMOIL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_GRAIN_ROUGH);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_GRAIN_POLISH_BROKEN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_GRAIN_POLISH_PARBOILED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_REFINED_SOYA_OIL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RAW_SOYA_OIL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SWEET_POTATO_UNPEELED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SWEET_POTATO_PEELED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WHEAT_HARD_GRAIN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WHEAT_SOFT_GRAIN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WATER_YAM_UNPEELED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WATER_YAM_PEELED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_YELLOW_YAM_UNPEELED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_YELLOW_YAM_PEELED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_YELLOW_YAM_PEELED_ONLY);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RIPED_PAWPAW_SEED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WATER_LEAF_SEEDS);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RIPED_PAWPAW_FRUIT_PEELED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RIPED_PLANTAIN_FRUIT);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_TOASTED_GROUNDNUT);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SOYBEAN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_COWPEA);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_ORANGE_PULP);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_MELON_FRUIT_PULP);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_ORANGE_PEELINGS);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_PLANTAIN_PEELS);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BITTER_LEAVES);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_YAM_FLOUR);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_PALM_OIL_SLUDGE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_EVAPORATED_PALM_OIL_);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_MAIZE_STARCH_RESIDUE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_AFRICAN_LOCUST);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_AFRICAN_LOCUST_BEAN_SEED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_AFRICAN_LOCUST_BEAN_WHOLE_FRUIT);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_ALMOND_KERNEL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BAMBARA_GROUNDNUT_DE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BAMBARA_GROUNDNUT_UN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BREWER_YEAST);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CASHEW_UN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CASHEW_EX);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_FULL_FAT);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_COTTON_SEED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_GROUNDNUT_CAKE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_GROUNDNUT_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_PIGEON_PEA);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_ROSELLE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SESAME_SEED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SUNFLOWER);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WATER_MELON_UN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WATER_MELON_SHELLED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BLOOD_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_EARTHWORM);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_FEATHER);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_GARDEN_SNAIL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_HOUSEFLY_MAGGOT);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_LOCAL_FISH_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_TERMITE_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_UNSKINNED_DRIED_TADPOLE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_UNSKINNED_BOILED_TADPOLE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BREWER_WET_GRAIN );
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BREWER_DRY_GRAIN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_CORN_COBS);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_COTTON_SEED_HULLS);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_PALM_KERNEL_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_POULTRY_MANURE_DRIED_CAGE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_POULTRY_MANURE_DRIED_FLOOR);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_OKRA_DRIED_FRUIT);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_HULLS);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_BRAN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_GRAIN_WITH_HULLS);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_GRAIN_HULL_REMOVED);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_OFFAL_UN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_RICE_OFFAL_PA);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_SORGHUM_OFFAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_TOMATO_PULP);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WHEAT_BRAN);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_WHEAT_OFFAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_OKRA);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_BONE_MEAL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_DICALCIUM_PHOSPHATE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_LIMESTONE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_OYSTER_SHELL);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_PERIWINKLE);
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_INSERT_EGG_SHELL_MEAL);
+
+
+// Inser initiate records
+        db.execSQL(ChickenFeedDbContract.IngredientRecord.SQL_INSERT_RECORD);
+
+    }
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(ChickenFeedDbContract.FeedLibraryEntry.SQL_DELETE_ENTRIES);
+        onCreate(db);
+
+    }
+//       insert id, feed_ingredient,class,crude_protein,energy,calcium,phosphorus
+
+    public Cursor getTable(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        //        Query statement to query the database
+        String selectQuery = "SELECT feed_ingredient FROM " + ChickenFeedDbContract.FeedLibraryEntry.TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        return cursor;
+    }
+
+    public Cursor getFeedNutrients(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        //        Query statement to query the database
+        String selectQuery = "SELECT * FROM " + ChickenFeedDbContract.FeedLibraryEntry.TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        return cursor;
+    }
+
+    public Cursor getFormuRecord(){
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String query = "SELECT * FROM " + ChickenFeedDbContract.IngredientRecord.TABLE_NAME;
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        return cursor;
+    }
+
+
+    public boolean updateFomulatedRecord(Integer formu_no){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ChickenFeedDbContract.IngredientRecord.COLUMN_FORMULATION,formu_no);
+
+        long checkifdata = db.update(ChickenFeedDbContract.IngredientRecord.TABLE_NAME,contentValues,null,null);
+
+        db.close();
+
+        if(checkifdata==-1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+//   add formulated ingredient to database
+
+    public boolean addIngredient(String ingredient, Integer formu_no, String birdcat, Integer qtyformulateed, String classOfIngredient, Double calcium,
+                                 Double crude_protein, Double Phosphorus, Integer energy){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_FEED_INGREDIENT,ingredient);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_FORMULATION_NO,formu_no);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_BIRD_CATEGORY,birdcat);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_QUANTITY_FORMULATED,qtyformulateed);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_CLASS,classOfIngredient);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_CRUDE_PROTEIN,crude_protein);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_ENERGY,energy);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_CALCIUM,calcium);
+        contentValues.put(ChickenFeedDbContract.FormulatedRecord.COLUMN_PHOSPHORUS,Phosphorus);
+
+
+        long checkifdata = db.insert(ChickenFeedDbContract.FormulatedRecord.TABLE_NAME,null,contentValues);
+        db.close();
+
+        if(checkifdata==-1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    public Cursor getIngredient(Integer formu){
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String query = "SELECT * FROM " + ChickenFeedDbContract.FormulatedRecord.TABLE_NAME +
+                " WHERE " + ChickenFeedDbContract.FormulatedRecord.COLUMN_FORMULATION_NO + "=?";
+
+        Cursor cursor = db.rawQuery(query, new String[]{String.valueOf(formu)});
+
+        return cursor;
+    }
+
+
+
+//    add result formulated to database
+
+    public boolean addFormulationResult(String feed_ingredient, Integer formu_no, Double proportionPercent,
+                                        Double proportionUnit, Integer qtySpecified, String comment) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(ChickenFeedDbContract.ResultFormulatedRecord.COLUMN_FEED_INGREDIENT, feed_ingredient);
+        contentValues.put(ChickenFeedDbContract.ResultFormulatedRecord.COLUMN_FORMULATION_NO, formu_no);
+        contentValues.put(ChickenFeedDbContract.ResultFormulatedRecord.COLUMN_PROPORTION_PERCENT, proportionPercent);
+        contentValues.put(ChickenFeedDbContract.ResultFormulatedRecord.COLUMN_PROPORTION_UNIT, proportionUnit);
+        contentValues.put(ChickenFeedDbContract.ResultFormulatedRecord.COLUMN_QUANTITY_SPECIFIED, qtySpecified);
+        contentValues.put(ChickenFeedDbContract.ResultFormulatedRecord.COLUMN_COMMENT, comment);
+
+        long checkifdata = db.insert(ChickenFeedDbContract.ResultFormulatedRecord.TABLE_NAME, null, contentValues);
+
+        db.close();
+
+        if (checkifdata == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //    add result formulated to database
+
+    public Cursor getFormulationResult(Integer formu) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String query = "SELECT * FROM " + ChickenFeedDbContract.ResultFormulatedRecord.TABLE_NAME +
+                " WHERE " + ChickenFeedDbContract.ResultFormulatedRecord.COLUMN_FORMULATION_NO + "=?";
+
+        Cursor cursor = db.rawQuery(query, new String[]{String.valueOf(formu)});
+
+        return cursor;
+    }
+
+}
